74f3c3583843d1ac3c1c2a085bc6a92589d8a570
Shehab Hossam <Shehab.haz@gmail.com>
2023-05-04 17:28:29 +0300

    added MVC / Json crud

---
+++ Nodejs/controllers/UserController.js
@@ -0,0 +1,76 @@
+const User = require('../models/User'); // import the User model
+
+// get all users
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// get a single user by ID
+const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.json(user);
+  } catch (err) {
+    res.status(404).json({ message: "User not found" });
+  }
+};
+
+// create a new user
+const createUser = async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  });
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// update a user by ID
+const updateUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      user.password = req.body.password || user.password;
+      const updatedUser = await user.save();
+      res.json(updatedUser);
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// delete a user by ID
+const deleteUser = async (req, res) => {
+  try {
+    const user = await User.deleteOne({_id : req.params.id});
+    if (user) {
+      res.json({ message: "User deleted" });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser
+};
---
+++ Nodejs/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const connectDB = async (dbName) => {
+  const dbUri = process.env.DB_URI || `mongodb://127.0.0.1:27017/${dbName}`;
+
+  try {
+    await mongoose.connect(dbUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log(`Connected to MongoDB: ${dbUri}`);
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
