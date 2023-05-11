9d68995b46d2f069543264514101649f72783e20
Mayar <mayarhamed15@gmail.com>
2023-05-12 00:28:02 +0300

    centering content

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameCardComponent } from './game-card/game-card.component';
+import { FiltersComponent } from './filters/filters.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameCardComponent
+    GameCardComponent,
+    FiltersComponent
   ],
   imports: [
     BrowserModule,
---
+++ Angular/src/app/filters/filters.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filters',
+  templateUrl: './filters.component.html',
+  styleUrls: ['./filters.component.css']
+})
+export class FiltersComponent {
+
+}
