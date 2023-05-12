463dd523514c8966ff84a23578182863485d3720
Mayar <mayarhamed15@gmail.com>
2023-05-12 04:31:18 +0300

    adding cart and fav buttons

--- Angular/src/app/game-card/game-card.component.ts
+++ Angular/src/app/game-card/game-card.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./game-card.component.css']
 })
 export class GameCardComponent {
+  isFavorite: boolean = false;
 
+  toggleFavorite(): void {
+    this.isFavorite = !this.isFavorite;
+  }
+
+  isAdded: boolean = false;
+
+  toggleAdded(): void {
+    this.isAdded = !this.isAdded;
+  }
 }
