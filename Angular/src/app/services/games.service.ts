bd6ce2b71d9ea087eb0cb5907933eaf5808e7e51
Shehab Hossam <Shehab.haz@gmail.com>
2023-05-20 13:18:57 +0300

    Edited Angular and run build command

--- Angular/src/app/services/games.service.ts
+++ Angular/src/app/services/games.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment  } from 'src/enviroments/enviroment';
+import { environment  } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
