47bee1f0d79fee29b75f81acf627c08f4c760c5f
Shehab Hossam <Shehab.haz@gmail.com>
2023-05-22 09:51:13 +0300

    fixed cart validation

--- Nodejs/controllers/CartController.js
+++ Nodejs/controllers/CartController.js
@@ -1,12 +1,12 @@
 const User = require("../models/User"); // import the User model
 const jwt = require("jsonwebtoken");
+const Joi = require("joi");
 
 
 const updateUserCart = async (req, res) => {
-    console.log("Hello from cart")
     const userId = req.params.id;
     const cart = req.body.cart;
-    console.log(userId);
+
     try {
       const user = await User.findById(userId);
   
@@ -17,6 +17,14 @@ const updateUserCart = async (req, res) => {
   
       // Update the cart for the user
       user.cart = cart;
+
+      const {error} = validateUpdate(req.body);
+      
+      if(error)
+      {
+        return res.status(400).json({ message: "Cart Must be array" });
+      }
+
       const updated =  await user.save();
       if(!updated)
       {
@@ -25,11 +33,17 @@ const updateUserCart = async (req, res) => {
       return res.json({ message: "Cart updated successfully" });
     } catch (error) {
       // Handle any errors that occurred
-      res.status(500).json({ message: "in catch "+error.message });
+      res.status(500).json({ message: error.message });
       return;
     }
   };
   
+const validateUpdate = (data) => {
+    const userCartSchema = Joi.object({
+      cart: Joi.array().required()
+    });
+    return userCartSchema.validate(data);
+  };
 module.exports = {
     updateUserCart
 }
