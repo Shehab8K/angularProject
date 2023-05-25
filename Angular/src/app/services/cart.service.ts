707761e21d1f2c206dc42ed2e6e7378d46a4d716
Raneen Mahmoud <raneenm260@gmail.com>
2023-05-25 19:52:27 +0200

    edit cart service

--- Angular/src/app/services/cart.service.ts
+++ Angular/src/app/services/cart.service.ts
@@ -6,9 +6,10 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class CartService {
-  
+
   private totalSubject: BehaviorSubject<number> = new BehaviorSubject<number>(0);
   private cartItems: BehaviorSubject<any[]> = new BehaviorSubject<any[]>([]);
+  // By convention, appending a dollar sign $ to the variable name indicates that it is an Observable
   public total$ = this.totalSubject.asObservable();
   public cartItems$: Observable<any[]> = this.cartItems.asObservable();
 
