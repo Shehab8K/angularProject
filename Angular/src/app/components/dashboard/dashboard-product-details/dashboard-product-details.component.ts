6ec825f3363eb5ee11d3ada8344f34aa51e6f63d
NadaAlaaEldeen <nada45200017@gmail.com>
2023-05-26 01:18:56 +0300

    add product details for dashboard

--- Angular/src/app/components/dashboard/dashboard-product-details/dashboard-product-details.component.ts
+++ Angular/src/app/components/dashboard/dashboard-product-details/dashboard-product-details.component.ts
@@ -8,6 +8,9 @@ import { GamesService } from 'src/app/services/products.service';
   styleUrls: ['./dashboard-product-details.component.css']
 })
 export class DashboardProductDetailsComponent implements OnInit {
+  Title: string="Game details";
+  hoveredImageUrl: string = ""; // Added hoveredImageUrl property
+  defaultImageUrl: string = "../../../../assets/images/giphy.gif"; // Set a default image URL
   ID:any;
   game:any;
   constructor(myRoute:ActivatedRoute,public myService:GamesService ){
@@ -15,13 +18,17 @@ export class DashboardProductDetailsComponent implements OnInit {
     // console.log(this.ID)
   }
   ngOnInit(): void {
-   this.myService.GetGameByID(this.ID).subscribe(
-    {
-      next:(data)=>{
-        this.game=data
-      },
-      error:(err)=>{console.log(err)}
-    }
-   )
+    this.myService.GetGameByID(this.ID).subscribe(
+      {
+        next: (data) => {
+          this.game = data;
+        },
+        error: (err) => { console.log(err); }
+      }
+    );
+  }
+
+  displayLargeImage(imageUrl: string) {
+    this.hoveredImageUrl = imageUrl;
   }
 }
