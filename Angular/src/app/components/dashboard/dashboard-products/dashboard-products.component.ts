cd7865f04b08f77f649f02f8b4f24e40770ec4df
asmaa <asmaagamal99192@gmail.com>
2023-05-26 17:41:43 +0300

    format date in products table

--- Angular/src/app/components/dashboard/dashboard-products/dashboard-products.component.ts
+++ Angular/src/app/components/dashboard/dashboard-products/dashboard-products.component.ts
@@ -50,4 +50,13 @@ export class DashboardProductsComponent implements OnInit {
   removeDeletedProduct(id: any) {
     this.games = this.games.filter(game => game._id !== id);
   }
+
+   formatDate(dateString: string): string {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');  
+    return `${day} - ${month} - ${year}`;
+  }
+
 }
