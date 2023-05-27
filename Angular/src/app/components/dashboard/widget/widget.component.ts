a3151f112ba96ab20316b3ae5ef42ac4b1e013a9
Raneen Mahmoud <raneenm260@gmail.com>
2023-05-27 20:24:56 +0200

    add data in dashboardhome

--- Angular/src/app/components/dashboard/widget/widget.component.ts
+++ Angular/src/app/components/dashboard/widget/widget.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { OrdersService } from 'src/app/services/orders.service';
 import { GamesService } from 'src/app/services/products.service';
+import { UserService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-widget',
@@ -7,9 +9,35 @@ import { GamesService } from 'src/app/services/products.service';
   styleUrls: ['./widget.component.css']
 })
 export class WidgetComponent {
-  constructor(private gamesService: GamesService) {
-    this.gamesService.GetAllGames();
+
+  itemsCount: number = 0
+  usersCount: number = 0
+  ordersCount: number = 0
+
+  constructor(private gamesService: GamesService, private userService: UserService, private orderService:OrdersService) {
+    this.getAllItemsCount();
+    this.getAllUsersCount();
+    this.getAllOrdersCount();
   }
 
+  getAllItemsCount() {
+    this.gamesService.GetAllGames().subscribe((items: Object) => {
+      this.itemsCount = Object.keys(items).length;
+      // console.log(`Number of items: ${this.itemsCount}`);
+    });
+  }
 
+  getAllUsersCount() {
+    this.userService.getAllUsers().subscribe((users: Object) => {
+      this.usersCount = Object.keys(users).length;
+      // console.log(`Number of items: ${this.usersCount}`);
+    });
+  }
+
+  getAllOrdersCount() {
+    this.orderService.getAllOrders().subscribe((orders: Object) => {
+      this.ordersCount = Object.keys(orders).length;
+      // console.log(`Number of items: ${this.usersCount}`);
+    });
+  }
 }
